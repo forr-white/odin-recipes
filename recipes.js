b0b005ecc4768fc1cde88d06aef60b37b5eada3a
Forrest White <29361908+forr-white@users.noreply.github.com>
2025-10-16 22:53:36 -0500

    Auto commit

--- recipes.js
+++ recipes.js
@@ -128,7 +128,7 @@ async function init() {
   displayRecipes(allRecipes);
 
   // Filter + Search listeners
-  document.querySelectorAll("#filter-category, #cuisineFilter, ##searchCombined")
+document.querySelectorAll("#filter-category, #cuisineFilter, #searchCombined")
     .forEach(el => el.addEventListener("input", updateDisplay));
 
   // Sort listener
