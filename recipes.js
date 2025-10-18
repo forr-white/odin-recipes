71df4540f5aaa248ec81f2fb82d9cf70a871c310
Forrest White <29361908+forr-white@users.noreply.github.com>
2025-10-18 00:15:27 -0500

    made header and searches transparent

--- recipes.js
+++ recipes.js
@@ -25,6 +25,15 @@ async function fetchRecipes() {
   }
 }
 
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
 function displayRecipes(recipes) {
   const container = document.getElementById("recipesContainer");
   container.innerHTML = "";
