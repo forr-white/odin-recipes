75232b4a566d20251a5b90db317523eab06eedd6
Forrest White <29361908+forr-white@users.noreply.github.com>
2025-12-09 00:38:03 -0600

    moved script to own file

--- recipes.js
+++ recipes.js
@@ -291,16 +291,3 @@ async function init() {
 }
 
 document.addEventListener("DOMContentLoaded", init);
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(reg => {
-        console.log('Service worker registered:', reg.scope);
-      })
-      .catch(err => {
-        console.error('Service worker registration failed:', err);
-      });
-  });
-}
